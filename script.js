626cfc3c0c9bc339647cde5b3e109b410c9ee428
Timothy Porter <tmporter42@gmail.com>
2019-12-29 02:23:05 -0800

    Add multiply layer

--- script.js
+++ script.js
@@ -8,34 +8,56 @@ function draw() {
   const template = new Image();
   template.crossOrigin = "anonymous";
 
+  const multiply = new Image();
+  multiply.crossOrigin = "anonymous";
+
   template.onload = function() {
-    const scale = Math.min(canvas.width / template.width, canvas.height / template.height);
+    multiply.onload = function() {
+      const scale = Math.min(canvas.width / template.width, canvas.height / template.height);
 
-    canvas.width = template.width * scale;
-    canvas.height = template.height * scale;
+      canvas.width = template.width * scale;
+      canvas.height = template.height * scale;
 
-    ctx.fillStyle = colorInput.value;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.globalCompositeOperation = "source-over";
+      ctx.drawImage(
+        multiply,
+        0,
+        0,
+        template.width,
+        template.height,
+        0,
+        0,
+        template.width * scale,
+        template.height * scale
+      );
 
-    ctx.drawImage(
-      template,
-      0,
-      0,
-      template.width,
-      template.height,
-      0,
-      0,
-      template.width * scale,
-      template.height * scale
-    );
+      ctx.fillStyle = colorInput.value;
+      ctx.globalCompositeOperation = "multiply";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    imageElement.width = canvas.width;
-    imageElement.height = canvas.height;
+      ctx.globalCompositeOperation = "source-over";
+      ctx.drawImage(
+        template,
+        0,
+        0,
+        template.width,
+        template.height,
+        0,
+        0,
+        template.width * scale,
+        template.height * scale
+      );
 
-    imageElement.src = canvas.toDataURL("image/png");
+      // render canvas to <img/>
+      imageElement.width = canvas.width;
+      imageElement.height = canvas.height;
+
+      imageElement.src = canvas.toDataURL("image/png");
+    };
   };
 
   template.src = "https://raw.githubusercontent.com/tmporter/dog-wif-hat-generator/master/images/template.png";
+  multiply.src = "https://raw.githubusercontent.com/tmporter/dog-wif-hat-generator/master/images/multiply.jpg";
 }
 
 function save() {
