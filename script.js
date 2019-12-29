9506d8decd0697f8d4b274fb6c2569183cca2612
Timothy Porter <tmporter42@gmail.com>
2019-12-29 01:50:59 -0800

    Add save button. Render to image element.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const colorInput = document.getElementById("color");
+const saveButton = document.getElementById("save");
+const imageElement = document.getElementById("image");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 function draw() {
   const template = new Image();
+  template.crossOrigin = "anonymous";
 
   template.onload = function() {
     const scale = Math.min(canvas.width / template.width, canvas.height / template.height);
@@ -25,11 +28,24 @@ function draw() {
       template.width * scale,
       template.height * scale
     );
+
+    imageElement.width = canvas.width;
+    imageElement.height = canvas.height;
+
+    imageElement.src = canvas.toDataURL("image/png");
   };
 
-  template.src = "./template.png";
+  template.src = "https://raw.githubusercontent.com/tmporter/dog-wif-hat-generator/master/template.png";
+}
+
+function save() {
+  const a = document.createElement("a");
+  a.href = canvas.toDataURL("image/png");
+  a.download = "dog_wif_hat.png";
+  a.click();
 }
 
 colorInput.addEventListener("change", () => draw());
+saveButton.addEventListener("click", () => save());
 
 draw();
